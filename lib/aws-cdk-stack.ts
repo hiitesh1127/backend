696070eb375840ff4250cefb186bfd6ff93d00bf
hiitesh1127 <hiteshkk49@gmail.com>
2023-03-20 05:17:25 -0400

    lint set up

--- lib/aws-cdk-stack.ts
+++ lib/aws-cdk-stack.ts
@@ -1,10 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib'
+import type { Construct } from 'constructs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor (scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
 
     // The code that defines your stack goes here
 
